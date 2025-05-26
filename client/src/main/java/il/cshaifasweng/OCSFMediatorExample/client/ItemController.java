bbc2b4df3cca1109c558ee1557d1b343bb86eecb
KanarMohana <kanarmohana@gmail.com>
2025-05-26 18:02:31 +0300

    catalogview

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ItemController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ItemController.java
@@ -22,7 +22,7 @@ public class ItemController {
         this.item = item;
         nameLabel.setText(item.getName());
         typeLabel.setText(item.getType());
-        priceLabel.setText(String.valueOf(item.getPrice()));
+        priceLabel.setText(String.valueOf(item.getPrice())+"₪");
         descriptionTextArea.setText(item.getDescription());
     }
 
